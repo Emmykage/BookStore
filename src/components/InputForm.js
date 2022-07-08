4bff877df6ab9c3d0d21af8e21e8d472db1501a2
Menanya <emmmiemenz@gmail.com>
2022-07-08 21:57:08 +0100

    fix linters

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -4,7 +4,7 @@ const InputForm = () => (
   <form>
     <input type="text" name="title" placeholder="Enter NAme of Book" />
     <input type="text" name="author" placeholder="Enter Name of AUthor" />
-    <button type='button'>Add Book</button>
+    <button type="button">Add Book</button>
 
   </form>
 );
