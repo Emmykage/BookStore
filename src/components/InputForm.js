eceb55cdb890be8e7024a31d931e16c6813c862c
Menanya <emmmiemenz@gmail.com>
2022-07-08 21:55:23 +0100

    add button to form

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -4,6 +4,7 @@ const InputForm = () => (
   <form>
     <input type="text" name="title" placeholder="Enter NAme of Book" />
     <input type="text" name="author" placeholder="Enter Name of AUthor" />
+    <button type='button'>Add Book</button>
 
   </form>
 );
